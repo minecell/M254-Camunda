400c4647fb0ea23658a083df536661f7adb26f51
minecell <dazanoli@gmail.com>
2018-01-27 06:59:24 +0100

    läuft

---
+++ src/main/java/org/camunda/bpm/example/usertask/embedded/serialized/delegate/ProcessRating.java
@@ -0,0 +1,49 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.example.usertask.embedded.serialized.delegate;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.example.usertask.embedded.serialized.model.CustomerData;
+
+/**
+ * @author Daniel Meyer
+ *
+ */
+public class ProcessRating implements JavaDelegate {
+
+  private final static Logger LOG = Logger.getLogger(ProcessRating.class.getName());
+
+  public void execute(DelegateExecution execution) throws Exception {
+
+    CustomerData customerData = (CustomerData) execution.getVariable("customerData");
+
+    LOG.info("\n\n\n    processing rating for customer "+customerData+"\n\n\n");
+
+    float max = 12.0f;
+    float current = (float)customerData.getAnswer1points() + (float)customerData.getAnswer2points() + (float)customerData.getAnswer3points();
+    
+    float currentPercent = (current / max)*100;
+    
+    LOG.info(Float.toString(current) + " von " + Float.toString(max));
+    LOG.info(Float.toString(currentPercent) + "%");
+    if (currentPercent < 75.0f) {
+    	LOG.info("Failed");
+    } else {
+    	LOG.info("Passed");
+    }
+  }
+
+}
--- src/main/java/org/camunda/bpm/example/usertask/embedded/serialized/model/CustomerData.java
+++ src/main/java/org/camunda/bpm/example/usertask/embedded/serialized/model/CustomerData.java
@@ -16,35 +16,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Represents a complex data object modeling customer data.
- * The data is stored as JSON which is automatically mapped to this java
- * object by the process engine.
+ * Represents a complex data object modeling customer data. The data is stored
+ * as JSON which is automatically mapped to this java object by the process
+ * engine.
  *
  * @author Daniel Meyer
  *
  */
 public class CustomerData {
 
-  protected String answer1;
-  protected String answer2;
+	protected String answer1;
+	protected String answer2;
+	protected String answer3;
+	protected int answer1points;
+	protected int answer2points;
+	protected int answer3points;
+	protected String name;
+	public String getAnswer1() {
+		return answer1;
+	}
 
-  public String getAnswer1() {
-    return answer1;
-  }
-  public void setAnswer1(String answer1) {
-    this.answer1 = answer1;
-  }
-  
-  public String getanswer2() {
-    return answer2;
-  }
-  public void setAnswer2(String answer2) {
-    this.answer2 = answer2;
-  }
+	public void setAnswer1(String answer1) {
+		this.answer1 = answer1;
+	}
 
+	public String getanswer2() {
+		return answer2;
+	}
 
-  public String toString() {
-    return "CustomerData [answer1=" + answer1 + ", answer2=" + answer2 + "]";
-  }
+	public void setAnswer2(String answer2) {
+		this.answer2 = answer2;
+	}
+
+	public String getanswer3() {
+		return answer3;
+	}
+
+	public void setAnswer3(String answer3) {
+		this.answer3 = answer3;
+	}
+/*********************************************************/
+	public int getAnswer1points() {
+		return answer1points;
+	}
+
+	public void setAnswer1points(int answer1points) {
+		this.answer1points = answer1points;
+	}
+
+	public int getAnswer2points() {
+		return answer2points;
+	}
+
+	public void setAnswer2points(int answer2points) {
+		this.answer2points = answer2points;
+	}
+
+	public int getAnswer3points() {
+		return answer3points;
+	}
+
+	public void setAnswer3points(int answer3points) {
+		this.answer3points = answer3points;
+	}
+	
+/*********************************************************/
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String toString() {
+		return "CustomerData ["+ answer1 + answer2 + answer3 + answer1points + answer2points + answer3points + name + " ]";
+	}
 
 }
